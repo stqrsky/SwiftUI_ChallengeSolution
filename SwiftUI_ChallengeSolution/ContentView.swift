2f2171203a8b088baf40672d2b6f784a252b197c
Starsky <starsky.m@live.com>
2022-04-19 17:45:09 +0200

    Initial Commit

---
+++ SwiftUI_ChallengeSolution/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_ChallengeSolution
+//
+//  Created by star on 19.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_ChallengeSolution/SwiftUI_ChallengeSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChallengeSolutionApp.swift
+//  SwiftUI_ChallengeSolution
+//
+//  Created by star on 19.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChallengeSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
