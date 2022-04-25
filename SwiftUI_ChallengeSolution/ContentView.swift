1aef354c3e81c42c2802e76a61eaf117282fd532
Starsky <starsky.m@live.com>
2022-04-25 15:33:33 +0200

    solution adding attributes, level easy

--- SwiftUI_ChallengeSolution/ContentView.swift
+++ SwiftUI_ChallengeSolution/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20.0) {
+            Text("1")
+                .padding()
+                .background(Color.red)
+                .clipShape(Circle())
+                .frame(width: 190, alignment: .leading)
+            Text("2")
+                .padding()
+                .background(Color.yellow)
+                .clipShape(Circle())
+                .frame(width: 190)
+            Text("3")
+                .padding()
+                .background(Color.green)
+                .clipShape(Circle())
+                .frame(width: 190, alignment: .trailing)
+        }
+        .font(.headline)
     }
 }
 
