1b7301364dda58e4d3dfe9c344e1b38f19ed0304
Starsky <starsky.m@live.com>
2022-04-25 16:45:49 +0200

    buttons

--- SwiftUI_ChallengeSolution/ContentView.swift
+++ SwiftUI_ChallengeSolution/ContentView.swift
@@ -8,36 +8,52 @@
 import SwiftUI
 
 struct RoundedNumber: ViewModifier {
-    let backgroundColor: Color
+    let backgroundColor: Color?
     func body(content: Content) -> some View {
         content
             .padding()
             .background(backgroundColor)
             .clipShape(Circle())
             .font(.headline)
+            .foregroundColor(.black)
     }
 }
 
 extension View {
-    func roundedNumber(backgroundColor: Color) -> some View {
+    func roundedNumber(backgroundColor: Color?) -> some View {
         self
             .modifier(RoundedNumber(backgroundColor: backgroundColor))
     }
 }
 
 struct ContentView: View {
+    var buttonsBackground: [Color] = [.red, .green, .yellow, .purple, .orange, .pink, Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1))]
+    
     var body: some View {
         VStack(spacing: 20.0) {
-            Text("1")
-                .roundedNumber(backgroundColor: .red)
-                .frame(width: 190, alignment: .leading)
-            Text("2")
-                .roundedNumber(backgroundColor: .yellow)
-                .frame(width: 190)
-            Text("3")
-                .roundedNumber(backgroundColor: .green)
-                .frame(width: 190, alignment: .trailing)
-        }
+            Button(action: {
+                    print("didTap 1")
+            }) {
+                Text("1")
+                    .roundedNumber(backgroundColor: buttonsBackground.randomElement())
+                    .frame(width: 190, alignment: .leading)
+            }
+            Button(action: {
+                    print("didTap 2")
+            }) {
+                Text("2")
+                    .roundedNumber(backgroundColor: buttonsBackground.randomElement())
+                    .frame(width: 190, alignment: .center)
+            }
+            Button(action: {
+                    print("didTap 3")
+            }) {
+                Text("3")
+                    .roundedNumber(backgroundColor: buttonsBackground.randomElement())
+                    .frame(width: 190, alignment: .trailing)
+            }
+            
+        } //VStack
         
     }
 }
